51efc7f4b4cb303b65b400536b621bddc73e15e4
travleev <anton.travleev@kit.edu>
2017-02-27 21:15:06 +0100

    Update in version after checking with python2 and 3

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # from distutils.core import setup
 from setuptools import setup
 
+
 setup(name='numjuggler',
       # version: X.Y.Z, where:
       #    X -- major version. Different major versions are not back-compatible.
@@ -9,11 +10,11 @@ setup(name='numjuggler',
       #    Y -- minor version. New minor version, when new function(s) added.
       #
       #    Z -- update, new update number when a bug is fixed.
-      version='2.24a.18',
+      version='2.25a.18',
       description='MCNP input file renumbering tool',
       author='A.Travleev',
       author_email='anton.travleev@kit.edu',
       packages=['numjuggler', ],
       # scripts = ['numjuggler/numjuggler'],
-      entry_points = {'console_scripts': ['numjuggler = numjuggler.main:main']},
+      entry_points={'console_scripts': ['numjuggler = numjuggler.main:main']},
       )
