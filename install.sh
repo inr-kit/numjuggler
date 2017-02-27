bff933546fce82a34a2f35dcda2c27d109ad8f8a
travleev <anton.travleev@kit.edu>
2017-02-27 20:49:48 +0100

    Use setuptools and pip for development install

--- install.sh
+++ install.sh
@@ -15,18 +15,12 @@
 
 # Uninstall previous version
 pip uninstall numjuggler;
+# install the package anew (this generates new script)
+pip install -v -e . 
 
-# prepare new distribution files 
+# prepare new distribution files. These are not needed 
+# for local installation, but might be useful for users
 python setup.py clean
 rm dist/*
 python setup.py sdist
-
-# if in virtualenv, do not use the --user option:
-if [ -z $VIRTUAL_ENV ]; then
-    user="--user"
-else
-    user=""
-fi;
-pip install -e . 
-# pip install dist/numjuggler*.tar.gz $user
 
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 setup(name='numjuggler',
       # version: X.Y.Z, where:
